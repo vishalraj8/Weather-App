b4be29dbf682412699b0d9425fd6dec7a7f50454
vishal rajbhar <gamingdark340@gmail.com>
2023-11-15 10:55:09 +0530

    update

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ let weather = {
   
     fetchWeather: function (city) {
       fetch(
-        " https://api.openweathermap.org/data/2.5/weather?q=paris&units=metric&appid=")
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
         .then((response) => {
           if (!response.ok) {
             alert("No weather found.");
